3765df62a5c7b82464b3de386c8d39702bfebec2
BasselHamadeh <basselhamadeh3@gmail.com>
2023-12-31 17:36:09 +0100

    Backend working

---
+++ server.js
@@ -0,0 +1,19 @@
+// Backend (Express Server)
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/xsd', (req, res) => {
+  const xsdData = {
+    data: ["Hallo ich bins Bassel! Hallo von Backend HIHI", "Zalung"],
+  };
+
+  res.json(xsdData);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
