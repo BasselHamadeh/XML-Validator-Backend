b4b59d602e50123f5a5ee3a298c230fc8bdbe6da
BasselHamadeh <basselhamadeh3@gmail.com>
2024-01-04 22:14:38 +0100

    working properly

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
 
 app.get('/xsd', (req, res) => {
   const xsdData = {
-    data: ["Hallo ich bins Bassel! Hallo von Backend"],
+    data: ["Hallo ich bins Bassel! Hallo von Backend"]
   };
 
   res.json(xsdData);
 });
 
-const PORT = 8080;
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.DB_URI, dbOptions)
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Error connecting to database:', err));
+
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
