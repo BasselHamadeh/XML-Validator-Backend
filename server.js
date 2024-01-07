783f6c4b1b6df43e69b4f1f4f25871269711e0cf
BasselHamadeh <basselhamadeh3@gmail.com>
2024-01-07 20:01:54 +0100

    everything connected with database

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
   user: 'postgres',
@@ -14,34 +15,32 @@ const pool = new Pool({
   port: 5432,
 });
 
-app.get('/users', async (req, res) => {
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.get('/users', async (req, res, next) => {
   try {
     const { rows } = await pool.query('SELECT * FROM user_table');
     res.json(rows);
   } catch (error) {
-    console.error('Fehler beim Abrufen der Benutzerdaten:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    next(error);
   }
 });
 
-app.post('/users', async (req, res) => {
+app.post('/users', async (req, res, next) => {
   const { username, email, status, sicherheitsgruppe, password } = req.body;
 
-  const client = await pool.connect();
   try {
-    await client.query('BEGIN');
+    await pool.query(
+      'INSERT INTO public.user_table(username, email, status, sicherheitsgruppe, password) VALUES($1, $2, $3, $4, $5)',
+      [username, email, status, sicherheitsgruppe, password]
+    );
 
-    await client.query('INSERT INTO public.user_table(username, email, status, sicherheitsgruppe, password) VALUES($1, $2, $3, $4, $5)',
-      [username, email, status, sicherheitsgruppe, password]);
-
-    await client.query('COMMIT');
     res.status(200).send('Benutzer erfolgreich hinzugefügt');
   } catch (error) {
-    await client.query('ROLLBACK');
-    console.error('Fehler beim Hinzufügen des Benutzers:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    client.release();
+    next(error);
   }
 });
 
