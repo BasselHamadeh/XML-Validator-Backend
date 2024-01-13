8505f23facfab480283c2553e97817790728f209
BasselHamadeh <basselhamadeh3@gmail.com>
2024-01-13 14:00:22 +0100

    connected to xsd_files table in database

--- server.js
+++ server.js
@@ -15,12 +15,20 @@ const pool = new Pool({
   port: 5432,
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connected to database');
+  release();
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-app.get('/users', async (req, res, next) => {
+app.get('/user', async (req, res, next) => {
   try {
     const { rows } = await pool.query('SELECT * FROM user_table');
     res.json(rows);
@@ -29,7 +37,17 @@ app.get('/users', async (req, res, next) => {
   }
 });
 
-app.post('/users', async (req, res, next) => {
+app.get('/xsd', async (req, res, next) => {
+  try {
+    const { rows } = await pool.query('SELECT xsd_file_name FROM xsd_files');
+    const xsdFileNames = rows.map(row => row.xsd_file_name);
+    res.json({ data: xsdFileNames });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/user', async (req, res, next) => {
   const { username, email, status, sicherheitsgruppe, password } = req.body;
 
   try {
