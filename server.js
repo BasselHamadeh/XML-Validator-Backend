eb07be1a25312415ba4437be87d35ebab951da97
BasselHamadeh <basselhamadeh3@gmail.com>
2024-02-13 17:43:11 +0100

    validate working without xsd, not connecting properly though

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const { parseString } = require('xml2js');
 require('dotenv').config();
 
 const app = express();
@@ -30,7 +31,49 @@ app.use('/user', userRouter);
 app.use('/login', userInfoRouter);
 app.use('/xsd', xsdRouter);
 
-const PORT = process.env.PORT;
+app.post('/validateWithoutXSD', async (req, res) => {
+  try {
+    const xmlData = req.body.xmlData;
+
+    validateWithoutXSD(xmlData)
+      .then((result) => {
+        res.json({ success: true, validationResults: result });
+      })
+      .catch((error) => {
+        console.error('Error during validation without XSD:', error);
+        res.status(400).json({ success: false, errors: ['Validation failed', ...error] });
+      });
+  } catch (error) {
+    console.error('Error during validation without XSD:', error);
+    res.status(500).json({ success: false, errors: ['Internal Server Error'] });
+  }
+});
+
+const validateWithoutXSD = async (xmlData) => {
+  return new Promise((resolve, reject) => {
+    if (!xmlData.trim()) {
+      reject(['No XML content provided']);
+      return;
+    }
+
+    parseString(xmlData, { explicitArray: false }, (err, result) => {
+      if (err) {
+        console.error('Error parsing XML:', err);
+        reject(['Error parsing XML', err.message]);
+      } else {
+        if (result && result.parsererror) {
+          const parserErrors = result.parsererror;
+          const errorMessage = parserErrors.trim();
+          reject([errorMessage]);
+        } else {
+          resolve(result);
+        }
+      }
+    });
+  });
+};
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}.`);
