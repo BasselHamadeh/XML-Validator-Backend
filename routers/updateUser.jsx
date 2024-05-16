fb7dd6f2518e70a114fe7edad1a96543493e7455
BasselHamadeh <basselhamadeh3@gmail.com>
2024-05-16 18:05:16 +0200

    database name update, handleValidationWithXSD improved

--- routers/updateUser.jsx
+++ routers/updateUser.jsx
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE_USER,
+  database: process.env.DB_DATABASE_XML_Validator,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
--- routers/userInfoRouter.jsx
+++ routers/userInfoRouter.jsx
@@ -6,7 +6,7 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE_USER,
+  database: process.env.DB_DATABASE_XML_Validator,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
